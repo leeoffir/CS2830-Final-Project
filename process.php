ee4a34bdbb34fd557268ed2b6ef37542d96eb675
Lee Offir <ljotyd@mail.missouri.edu>
2017-04-29 17:42:39 -0500

    First Commit

---
+++ process.php
@@ -0,0 +1,23 @@
+<?php
+
+    $correctUser = 'test';
+    $correctPass = 'pass';
+
+    if(isset($_POST['submit'])){
+        $user = htmlspecialchars($_POST['user']); 
+        $pass =  htmlspecialchars($_POST['password']);
+        if($user == $correctUser){
+            if($pass == $correctPass){
+                 echo "You've been logged in";
+            } else {
+                echo "incorrect password";
+            }
+        } else {
+            echo "invalid user";
+        } 
+    }
+
+	
+
+    
+?>
