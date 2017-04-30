c0e2dfb7df156608016c47a13b7ffc060e48074f
Lee Offir <ljotyd@mail.missouri.edu>
2017-04-29 21:31:23 -0500

    new

--- scripts/profilescripts.php
+++ scripts/profilescripts.php
@@ -80,7 +80,7 @@ function print_modal(){
               <div class="modal-content">
                 <div class="modal-header">
                   <button type="button" class="close" data-dismiss="modal">&times;</button>
-                  <h4 class="text-center modal-title">Modal Header</h4>
+                  <h4 class="center-block modal-title">Modal Header</h4>
                 </div>
                 <div class="modal-body">
                   <p>Some text in the modal.</p>
