9e6e35fb142a1d1d8a7a9a19768c8f8d3ea10b03
Lee Offir <ljotyd@mail.missouri.edu>
2017-04-29 19:01:11 -0500

    progress

--- profile.php
+++ profile.php
@@ -2,18 +2,54 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <title>Your Profile</title>
+    <title>Tom's Info</title>
   </head>
   <body>
     <div class="container">
       <div class="jumbotron">
 
         <div class="row">
-          <h2>Welcome to your profile</h2>
+          <h1>Tom Brady's Full Profile</h1>
+        </div>
+        <!-- first row -->
+        <div class="row">
+          <div class="col-md-4">
+            <div class="panel panel-default">
+              <div class="panel-heading">1</div>
+              <div class="panel-body">Panel Content</div>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="panel panel-default">
+              <div class="panel-heading">2</div>
+              <div class="panel-body">Panel Content</div>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="panel panel-default">
+              <div class="panel-heading">3</div>
+              <div class="panel-body">Panel Content</div>
+            </div>
+          </div>
+        </div>
+        <!-- Second Row -->
+        <div class="row">
+          <div class="col-md-offset-2 col-md-4">
+            <div class="panel panel-default">
+              <div class="panel-heading">4</div>
+              <div class="panel-body">Panel Content</div>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="panel panel-default">
+              <div class="panel-heading">5</div>
+              <div class="panel-body">Panel Content</div>
+            </div>
+          </div>
         </div>
 
 
-      </div>
+      </div><!-- end container and jumbotron-->
     </div>
   </body>
 </html>
