918f0bd177f2c619a95af2ff5c588ce04834bc2d
Lee Offir <ljotyd@mail.missouri.edu>
2017-05-02 12:29:11 -0500

    news

--- index.php
+++ index.php
@@ -19,7 +19,7 @@ print_modal();
   <div class="container">
     <div class="jumbotron">
       <div class="row">
-        <img class="img-responsive" src="pats.gif"alt="Tom Brady">
+        <img class="img-responsive text-center" src="pats.gif"alt="Tom Brady">
       </div>
       <div class="row">
         <h1 class="text-center">Tom Brady's Fan Page!</h1>
